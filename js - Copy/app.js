f35cd0124db50275552418febadb5e608ddcba0c
Aldrin Dela Vega <aldrindelavega12@gmail.com>
2026-07-16 22:01:52 +0800

    update

---
+++ js - Copy/app.js
@@ -0,0 +1,38 @@
+async function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    try {
+        const response = await fetch("http://192.168.1.19:5001/api/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ username, password })
+        });
+
+        const data = await response.json();
+        console.log(data); // debug
+
+        if (data.status === "success") {
+            localStorage.setItem("user", JSON.stringify(data.user));
+
+            if (data.user.role === "member") {
+                window.location.href = "member.html";
+            } 
+            else if (data.user.role === "staff") {
+                window.location.href = "staff.html";
+            } 
+            else if (data.user.role === "admin") {
+                window.location.href = "admin.html";
+            }
+
+        } else {
+            document.getElementById("error").innerText = data.message;
+        }
+
+    } catch (error) {
+        console.error(error);
+        document.getElementById("error").innerText = "Server error";
+    }
+}
